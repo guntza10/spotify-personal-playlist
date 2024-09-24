20d41af10a47b2ca8421cbf941c09855bc23ecfe
guntza10 <jomphop.saibuatong@hotmail.com>
2024-09-24 16:44:47 +0700

    (update) set up global css for this project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React from "react";
+
 import "./styles/global.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return <div></div>;
+};
 
 export default App;
