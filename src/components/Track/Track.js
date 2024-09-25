5d10c0fd59b28456cbebc4920bb5366448e21629
guntza10 <jomphop.saibuatong@hotmail.com>
2024-09-25 17:03:45 +0700

    (add) implement Track component

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,7 +1,22 @@
 import React from "react";
 
-function Track() {
-  return <div>Track</div>;
-}
+import "./Track.css";
+
+const Track = React.memo(({ track }) => {
+  return (
+    <div className="track-container pb-2 mb-2">
+      <div className="track-information">
+        <h3 className="mb-_5">{track.name}</h3>
+        <p className="track-info">
+          <span className="mr-1">{track.artist}</span>
+          <span className="mr-1">|</span>
+          <span>{track.album}</span>
+        </p>
+      </div>
+
+      <button className="track-action">+</button>
+    </div>
+  );
+});
 
 export default Track;
