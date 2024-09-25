613a1945d273b6e46b6ceb463690ce0b94a1c310
guntza10 <jomphop.saibuatong@hotmail.com>
2024-09-25 21:29:09 +0700

    (add) implement Home component page

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "../../layouts/Header/Header";
+import Main from "../../layouts/Main/Main";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  );
+};
+
+export default Home;
