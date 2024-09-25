964fa475fc2dc0fb6f356fe8995ad13afe7e4e22
guntza10 <jomphop.saibuatong@hotmail.com>
2024-09-25 23:11:35 +0700

    (update) change constant REDIRECT_URI

--- src/configs/constant.js
+++ src/configs/constant.js
@@ -1,11 +1,19 @@
 const SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize";
 const CLIENT_ID = "cbff0df1d3324cf6913ce933a2c8d6c5";
 const CLIENT_SECRET = "eb22338df7154abd92f4b74cb4cab2c8";
-const REDIRECT_URI = "http://localhost:3001/home";
+const REDIRECT_URI = "http://localhost:3000/callback";
 const SCOPES = [
   "user-read-private",
   "playlist-modify-public",
   "playlist-modify-private",
 ];
+const SPOTIFY_SERVICE_URL = "https://api.spotify.com/v1";
 
-export { SPOTIFY_AUTH_URL, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, SCOPES };
+export {
+  SPOTIFY_AUTH_URL,
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI,
+  SCOPES,
+  SPOTIFY_SERVICE_URL,
+};
