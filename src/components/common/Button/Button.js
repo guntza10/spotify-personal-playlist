31bae1156897f24e10922ffb808aef774f17cc64
guntza10 <jomphop.saibuatong@hotmail.com>
2024-09-28 20:42:31 +0700

    (update) move style button from global to Button css

--- src/components/common/Button/Button.js
+++ src/components/common/Button/Button.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./Button.css";
+
 const Button = React.memo(({ className, children, ...props }) => {
   return (
     <button {...props} className={`button ${className}`}>
