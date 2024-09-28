f144ca0274b1f1b970185e2ddb48fb2911e959a6
guntza10 <jomphop.saibuatong@hotmail.com>
2024-09-28 19:38:44 +0700

    (add) implement Modal with react-modal

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import ReactModal from "react-modal";
+
+import "./Modal.css";
+
+const modalStyles = {
+  content: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    transform: "translate(-50%, -50%)",
+    padding: "24px",
+    border: "none",
+  },
+  overlay: {
+    backgroundColor: "rgba(var(--base-black-rgb),0.4)",
+  },
+};
+
+const Modal = React.memo(
+  ({ isOpen, onClose, children, contentLabel, ...props }) => {
+    return (
+      <ReactModal
+        {...props}
+        isOpen={isOpen}
+        onRequestClose={onClose}
+        contentLabel={contentLabel ? contentLabel : "Modal"}
+        style={modalStyles}
+      >
+        {children}
+      </ReactModal>
+    );
+  }
+);
+
+export default Modal;
