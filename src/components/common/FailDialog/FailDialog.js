784c4d2749a8c670dd5a4b6510a9f630d328ca94
guntza10 <jomphop.saibuatong@hotmail.com>
2024-09-29 14:52:11 +0700

    (add) implement FailDialog

---
+++ src/components/common/FailDialog/FailDialog.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import Button from "../Button/Button";
+import Modal from "../Modal/Modal";
+
+import { MdCancel } from "react-icons/md";
+
+import "./FailDialog.css";
+
+const FailDialog = React.memo(({ isOpen, message, onClose }) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} contentLabel="Fail Dialog">
+      <div className="dialog">
+        <div className="dialog-icon mb-1">
+          <MdCancel size={35} className="icon" />
+        </div>
+        <h2 className="mb-1">{message.heading}</h2>
+        <p className="mb-3">{message.description}</p>
+        <Button className="close-btn" onClick={onClose}>
+          Close
+        </Button>
+      </div>
+    </Modal>
+  );
+});
+
+export default FailDialog;
