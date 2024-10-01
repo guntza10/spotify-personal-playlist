91ea92298f276228dd8a5e55b3068c257ebd9d3a
guntza10 <jomphop.saibuatong@hotmail.com>
2024-10-02 02:10:35 +0700

    (update) implement preview button instead click entire card and change add and delete track to use icon instead

--- src/components/feature/Track/Track.js
+++ src/components/feature/Track/Track.js
@@ -1,5 +1,11 @@
 import React from "react";
 
+import Button from "../../common/Button/Button";
+
+import { FaHeadphonesAlt } from "react-icons/fa";
+import { IoMdAddCircleOutline } from "react-icons/io";
+import { FiMinusCircle } from "react-icons/fi";
+
 import "./Track.css";
 
 const Track = React.memo(
@@ -16,22 +22,26 @@ const Track = React.memo(
 
     return (
       <div {...props} className={`track-container pb-2 mb-2 ${className}`}>
-        <div className="track-information" onClick={handleOpenPreviewTrack}>
+        <div>
           <h3 className="mb-_5">{track.name}</h3>
-          <p className="track-info">
+          <p className="track-info mb-1">
             <span className="mr-1">{track.artist}</span>
             <span className="mr-1">|</span>
             <span>{track.album}</span>
           </p>
+          <Button className="preview-btn" onClick={handleOpenPreviewTrack}>
+            <FaHeadphonesAlt />
+            <span>PREVIEW</span>
+          </Button>
         </div>
 
         {onAddTrack ? (
           <button className="track-action" onClick={handleAddTrack}>
-            +
+            <IoMdAddCircleOutline size={25} />
           </button>
         ) : (
           <button className="track-action" onClick={handleRemoveTrack}>
-            -
+            <FiMinusCircle size={23} />
           </button>
         )}
       </div>
